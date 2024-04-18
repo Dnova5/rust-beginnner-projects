e2104f87b6eaf576848499a2f35a392779165dcf
dagi nova <154723539+Dnova5@users.noreply.github.com>
2024-04-17 18:34:25 -0700

    Update main2.rs

--- p1-99-bottles/src/main2.rs
+++ p1-99-bottles/src/main2.rs
@@ -1,13 +1,27 @@
-fn main(){
-  for bottles in 1..101  {
-    if bottles < 100 {
-      println!("Take one down and pass it around,  {} of beer on the wall.", 100 - bottles);
-    } else {
-      println!("No more bottles of beer on the wall");
-      println!("no more bottles of beer.");
-      println!("We've taken them down");
-      println!("and passed them around");
-      println!("now  we're drunk and passed out!");
-    }
-  }
+
+fn
+main ()
+{
+  let mut reminder = 100;
+  for bottles
+	in 1. .101
+	{
+	  if bottles
+		<100
+		{
+		  println !
+			("{} bottles of beer on the wall,  Take one down and pass it around,{} of beer on the wall.",
+			 reminder, 100 - bottles);
+		  reminder = reminder - 1;
+		}
+	  else
+		{
+		  println ! ("No more bottles of beer on the wall");
+		  println ! ("no more bottles of beer.");
+		  println ! ("We've taken them down");
+		  println ! ("and passed them around");
+		  println ! ("now  we're drunk and passed out!");
+		}
+
+	}
 }
